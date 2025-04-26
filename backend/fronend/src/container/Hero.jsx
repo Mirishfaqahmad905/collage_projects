79db66873df0a501b0ebdf3e0f555ce9f71eac93
mirishfaqahmad <140787979+Mirishfaqahmad905@users.noreply.github.com>
2025-04-26 09:05:11 -0700

    Update Hero.jsx

--- backend/fronend/src/container/Hero.jsx
+++ backend/fronend/src/container/Hero.jsx
@@ -5,8 +5,8 @@ const Hero = () => {
   const [currentIndex, setCurrentIndex] = useState(0);
   const slides = [
     'https://lh3.googleusercontent.com/p/AF1QipOKYUSMgVhH65rpLD_S0fN7E_Nn5H06skrFACBT=s1360-w1360-h1020',
-    'https://scontent.fisb9-1.fna.fbcdn.net/v/t39.30808-6/461772643_551531807222501_3287055501509344859_n.jpg?stp=dst-jpg_s720x720&_nc_cat=111&ccb=1-7&_nc_sid=833d8c&_nc_ohc=n1SY0qxuKLwQ7kNvgH5umP3&_nc_zt=23&_nc_ht=scontent.fisb9-1.fna&_nc_gid=A9eiJqns-XsMWyiD1CxwWjV&oh=00_AYDV9ZavJSe2sgYXNkw2Gupw8yHh-YO6jLRssi7XdyZVog&oe=6724147F',
-    'https://scontent.fisb9-1.fna.fbcdn.net/v/t39.30808-6/386600561_345196567856027_1208787795912616621_n.jpg?stp=dst-jpg_s960x960&_nc_cat=110&ccb=1-7&_nc_sid=cc71e4&_nc_ohc=fJLwUJIyj74Q7kNvgFWEoI7&_nc_zt=23&_nc_ht=scontent.fisb9-1.fna&_nc_gid=AhAyVnalxrVcEnrmWaqvloU&oh=00_AYCzZZFLea4GXbmJ6oAej6h3jAWSMTHZdL0e6qe6ow3RQQ&oe=67244428'
+    'https://www.facebook.com/photo/?fbid=551531747222507&set=a.191948613180824',
+    'https://www.facebook.com/photo/?fbid=520100810365601&set=pcb.520101490365533'
   ];
 
   // Automatically transition to the next slide every 3 seconds
