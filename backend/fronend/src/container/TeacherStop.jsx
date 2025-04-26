e85dd4dc664ad9274d2b9cc562cd02844c900e7f
mirishfaqahmad <140787979+Mirishfaqahmad905@users.noreply.github.com>
2025-04-26 04:29:48 -0700

    Update TeacherStop.jsx

--- backend/fronend/src/container/TeacherStop.jsx
+++ backend/fronend/src/container/TeacherStop.jsx
@@ -7,7 +7,7 @@ const TeacherStop = () => {
 
   const fetchData = async () => {
     try {
-      const response = await axios.get("http://localhost:3000/getData");
+      const response = await axios.get("https://git-jcs-backend.vercel.app/getData");
       setData(response.data); // Set the fetched data (array) into state
       console.log(response.data); // Logging the data for verification
     } catch (err) {
